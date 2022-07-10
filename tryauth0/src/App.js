5687f6d979db1762650b01b028458a6f21c6822c
Juan <jam20582@gmail.com>
2022-07-10 20:38:13 -0300

    loading added

--- tryauth0/src/App.js
+++ tryauth0/src/App.js
@@ -3,14 +3,30 @@ import React from 'react';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 import { useAuth0} from '@auth0/auth0-react';
+import loading from './Images/loading.gif';
 
 function App() {
-  return (
-    <div className="App">
-      {}
-      <LoginButton />
-      <LogoutButton />
+  const { isAuthenticated, user, isLoading } = useAuth0();
+    
+  if (isLoading) {
+    return <img src={loading} alt="Loading..." />;
+  }
 
+  return (
+
+    <div className="App">
+      {isAuthenticated ?
+      <div>
+        <h1>Welcome {user.name}!</h1>
+        <img src={user.picture} alt="profile" />
+        <div>
+          <LogoutButton />
+        </div>
+      </div> : 
+      <div>
+        <h1>Hi stranger! please log in to identify yourself</h1>
+        <LoginButton />
+      </div>}
     </div>
   );
 }
